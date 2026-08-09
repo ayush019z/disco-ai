fe7b02639e0c80e949d44e4ec4408d3f12cba14d
ayush019z <147528223+ayush019z@users.noreply.github.com>
2026-08-09 18:34:21 +0530

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('info')
+    .setDescription('Show information about the bot')
+    .toJSON()
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log('Registering slash commands...');
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log('Slash commands registered.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
