0ad45ee5733a19ecc3bad60b6b49dad0b468c8d5
ayush019z <147528223+ayush019z@users.noreply.github.com>
2026-08-10 02:02:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ const startTime = Date.now();
 // =========================
 // IMAGE COOLDOWN
 // =========================
-const cooldowns = new Map();
 
 // =========================
 // MEMORY (2 HOURS)
